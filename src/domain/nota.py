a29e44ce70df2b7afe4a4acfca65daccbfba7e44
Leo <151567681+Leuo29@users.noreply.github.com>
2025-12-17 09:30:43 -0300

    Delete src/domain directory

--- src/domain/nota.py
+++
@@ -1,6 +0,0 @@
-from .disciplina import Disciplina
-
-class Nota:
-    def __init__(self, disciplina, valor):
-        self.disciplina = disciplina
-        self.valor = valor
--- src/domain/strategies/curso_cr_strategy.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class CursoCRStrategy(ABC):
-    @abstractmethod
-    def calcular_media(self, curso, ano=None, semestre=None):
-        pass
